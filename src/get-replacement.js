ee5f86521fe68144a2030aa0b26b54d08331f713
Dang <huynhminhdang@gmail.com>
2018-05-30 16:27:57 +0800

    test

--- src/get-replacement.js
+++ src/get-replacement.js
@@ -7,7 +7,7 @@ function getReplacement({string: stringToPreval, filename, babel}) {
   const {code} = babel.transform(stringToPreval, {
     filename,
   })
-  const transpiled = `require('babel-register');\n${code}`
+  const transpiled = `require('@babel/register');\n${code}`
   const val = requireFromString(transpiled, filename)
   return objectToAST(val)
 }
--- src/object-to-ast.js
+++ src/object-to-ast.js
@@ -1,4 +1,4 @@
-const babylon = require('babylon')
+const babylon = require('@babel/parser')
 
 module.exports = objectToAST
 
